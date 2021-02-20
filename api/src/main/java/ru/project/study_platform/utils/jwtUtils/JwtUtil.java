fea675e10e48a451d76664063c4437176712747e
yank0vy3rdna <yankovyerdna@yandex.ru>
2021-02-20 07:34:04 +0300

    feat: govno

--- api/src/main/java/ru/project/study_platform/utils/jwtUtils/JwtUtil.java
+++ api/src/main/java/ru/project/study_platform/utils/jwtUtils/JwtUtil.java
@@ -2,12 +2,9 @@ package ru.project.study_platform.utils.jwtUtils;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.function.Function;
 
 @Component
