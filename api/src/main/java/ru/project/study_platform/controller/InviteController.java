50e24a91f4b468078a00216593c4cc73a58dd4b3
yank0vy3rdna <yankovyerdna@yandex.ru>
2021-02-20 06:58:49 +0300

    feat: invite controllers

--- api/src/main/java/ru/project/study_platform/controller/InviteController.java
+++ api/src/main/java/ru/project/study_platform/controller/InviteController.java
@@ -1,4 +1,38 @@
 package ru.project.study_platform.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.project.study_platform.model.dto.UrlDTO;
+import ru.project.study_platform.model.entity.GroupRoom;
+import ru.project.study_platform.model.entity.User;
+import ru.project.study_platform.repository.GroupRoomRepository;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
 public class InviteController {
+    private final GroupRoomRepository groupRoomRepository;
+
+    public InviteController(GroupRoomRepository groupRoomRepository) {
+        this.groupRoomRepository = groupRoomRepository;
+    }
+    @GetMapping("/group/generate_invite")
+    public UrlDTO generate(@RequestParam Long groupId, HttpServletRequest request, HttpServletResponse response){
+        User user = (User) request.getAttribute("user");
+        if (user.isTeacher()) {
+            GroupRoom groupRoom = groupRoomRepository.getOne(groupId);
+            return new UrlDTO("https://yank0vy3rdna.ru/study_platform/api/group/invite?hashId=" + groupRoom.getHashId());
+        }else
+            response.setStatus(409);
+        return null;
+    }
+
+    @GetMapping("/group/invite")
+    public void invite(@RequestParam String hashId, HttpServletRequest request) {
+        User user = (User) request.getAttribute("user");
+        GroupRoom groupRoom = groupRoomRepository.findGroupRoomByHashId(hashId);
+        groupRoom.getUsers().add(user);
+    }
 }
--- api/src/main/java/ru/project/study_platform/controller/MessageController.java
+++ api/src/main/java/ru/project/study_platform/controller/MessageController.java
@@ -7,27 +7,49 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.project.study_platform.model.dto.MessageDTO;
+import ru.project.study_platform.model.entity.GroupRoom;
 import ru.project.study_platform.model.entity.Message;
+import ru.project.study_platform.model.entity.User;
+import ru.project.study_platform.repository.GroupRoomRepository;
 import ru.project.study_platform.service.entityServices.messageService.MessageService;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Controller
 public class MessageController {
     private final SimpMessagingTemplate template;
     private final MessageService messageService;
+    private final GroupRoomRepository groupRoomRepository;
 
-    public MessageController(SimpMessagingTemplate messagingTemplate, MessageService messageService) {
+    public MessageController(SimpMessagingTemplate messagingTemplate, MessageService messageService, GroupRoomRepository groupRoomRepository) {
         this.template = messagingTemplate;
         this.messageService = messageService;
+        this.groupRoomRepository = groupRoomRepository;
     }
+
     @GetMapping("/messages")
-    public List<Message> getMessages(@RequestParam Long groupId){
-        return messageService.getMessagesByGroupId(groupId);
+    public List<Message> getMessages(@RequestParam Long groupId, HttpServletRequest request, HttpServletResponse response) {
+        User user = (User) request.getAttribute("user");
+        GroupRoom groupRoom = groupRoomRepository.getOne(groupId);
+        if (groupRoom.getUsers().contains(user))
+            return messageService.getMessagesByGroupId(groupId);
+        else {
+            response.setStatus(409);
+            return null;
+        }
     }
+
     @MessageMapping("/messages")
-    public void processMessage(@Payload MessageDTO message) {
-        messageService.createMessage(message);
-        template.convertAndSendToUser(message.getGroupId().toString(),"/messages", "new message lolb");
+    public void processMessage(@Payload MessageDTO message, HttpServletRequest request, HttpServletResponse response) {
+        User user = (User) request.getAttribute("user");
+        GroupRoom groupRoom = groupRoomRepository.getOne(message.getGroupId());
+        if (groupRoom.getUsers().contains(user)) {
+            messageService.createMessage(message);
+            template.convertAndSendToUser(message.getGroupId().toString(), "/messages", "new message lolb");
+        } else {
+            response.setStatus(409);
+        }
     }
 }
